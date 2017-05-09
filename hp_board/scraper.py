5f684c5655d52cc646c8d9d66ecf733c59ccfe43
Amanpreet Singh <apsdehal@gmail.com>
2017-05-09 11:45:14 -0400

    Fix style issues and add multiprocessing

--- hp_board/scraper.py
+++ hp_board/scraper.py
@@ -1,4 +1,9 @@
-import optparse, re
+import optparse
+import re
+from bs4 import BeautifulSoup
+
+import multiprocessing
+from joblib import Parallel, delayed
 
 try:
     from mechanize import Browser
@@ -6,7 +11,7 @@ except ImportError:
     print "mechanize required but missing"
     sys.exit(1)
 
-#Constants
+# Constants
 WEBSITE = 'http://www.hpbose.org/Result/MatricResult.aspx'
 ROLL_NO_HOLDER = "ctl00$ContentPlaceHolder1$txtRollNo"
 FORM_NAME = "aspnetForm"
@@ -17,97 +22,96 @@ br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; \
           rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 br.set_handle_robots(False)
 
-'''
-Regex for getting data in between span anchors
-'''	
-def getBWAnchors( part ):
-	return re.findall(r'>(.*?)<', part)	
 
-'''
-Get result of specific roll no and return the html response
-'''
+# Regex for getting data in between span anchors
+def getBWAnchors(part):
+    return re.findall(r'>(.*?)<', part)
+
+
+# Get result of specific roll no and return the html response
 def getResult(roll):
-	br.open(WEBSITE)
-	br.select_form(name=FORM_NAME)
-	br[ROLL_NO_HOLDER] = roll 
-	br.form.action = WEBSITE
-	response = br.submit()
+    try:
+        br.open(WEBSITE)
+        br.select_form(name=FORM_NAME)
+        br[ROLL_NO_HOLDER] = roll
+        br.form.action = WEBSITE
+        response = br.submit()
 
-	result = response.read()
-	return result
+        result = response.read()
+    except:
+        return getResult(roll)
+    print '[+] Got roll no. ' + roll
+    return result
 
-'''
-Just a regular workaround for getting data as I need
-'''
+
+# Just a regular workaround for getting data as I need
 def writeToFile(result):
-	write = open('result.html', 'w+')
-	write.write(result)
-	write.close()
+    write = open('result.html', 'w+')
+    write.write(result)
+    write.close()
 
-'''
-Now add data to the data variable already created
-'''
-def addData(data):
-	lines = open('result.html', 'r')
-	lines = lines.readlines()
-	try:
-		name = getBWAnchors(lines[321])[0]
-		rollno = getBWAnchors(lines[327])[0]
-		english = getBWAnchors(lines[396])[0]
-		math = getBWAnchors(lines[417])[0]
-		hindi = getBWAnchors(lines[432])[0]
-		sst = getBWAnchors(lines[447])[0]
-		science = getBWAnchors(lines[468])[0]
-		sanskrit = getBWAnchors(lines[477])[0]
-		art = getBWAnchors(lines[498])[0]
-		result = getBWAnchors(lines[408])[0]
-		data += '<tr>'
-		data += '<td>'+ rollno + '</td>'
-		data += '<td>'+ name +'</td>'
-		data += '<td>'+ english +'</td>'
-		data += '<td>'+ math +'</td>'
-		data += '<td>'+ hindi +'</td>'
-		data += '<td>'+ sst +'</td>'
-		data += '<td>'+ science +'</td>'
-		data += '<td>'+ sanskrit +'</td>'
-		data += '<td>'+ art +'</td>'
-		data += '<td>'+ result +'</td>'
-		data += '</tr>'
-	except:
-		pass	
-	return data
 
-'''
-Get initial data with table tags
-'''
+# Now add data to the data variable already created
+def addData(data, result):
+    result = result.split('\n')
+    try:
+        name = getBWAnchors(lines[321])[0]
+        rollno = getBWAnchors(lines[327])[0]
+        english = getBWAnchors(lines[396])[0]
+        math = getBWAnchors(lines[417])[0]
+        hindi = getBWAnchors(lines[432])[0]
+        sst = getBWAnchors(lines[447])[0]
+        science = getBWAnchors(lines[468])[0]
+        sanskrit = getBWAnchors(lines[477])[0]
+        art = getBWAnchors(lines[498])[0]
+        result = getBWAnchors(lines[408])[0]
+        data += '<tr>'
+        data += '<td>' + rollno + '</td>'
+        data += '<td>' + name + '</td>'
+        data += '<td>' + english + '</td>'
+        data += '<td>' + math + '</td>'
+        data += '<td>' + hindi + '</td>'
+        data += '<td>' + sst + '</td>'
+        data += '<td>' + science + '</td>'
+        data += '<td>' + sanskrit + '</td>'
+        data += '<td>' + art + '</td>'
+        data += '<td>' + result + '</td>'
+        data += '</tr>'
+    except:
+        pass
+    return data
+
+
+# Get initial data with table tags
 def getInitialData():
-	data = '<table>'
-	data += '<tr>'
-	data += '<th>Rollno</th>'
-	data += '<th>Name</th>'
-	data += '<th>English</th>'
-	data += '<th>Math</th>'
-	data += '<th>Hindi</th>'
-	data += '<th>SST</th>'
-	data += '<th>Science</th>'
-	data += '<th>Sanskrit</th>'
-	data += '<th>Art/Comp</th>'
-	data += '<th>Result</th>'
-	data += '</tr>'
-	return data
+    data = '<table>'
+    data += '<tr>'
+    data += '<th>Rollno</th>'
+    data += '<th>Name</th>'
+    data += '<th>English</th>'
+    data += '<th>Math</th>'
+    data += '<th>Hindi</th>'
+    data += '<th>SST</th>'
+    data += '<th>Science</th>'
+    data += '<th>Sanskrit</th>'
+    data += '<th>Art/Comp</th>'
+    data += '<th>Result</th>'
+    data += '</tr>'
+    return data
 
-data = getInitialData()
-for i in range(140659821, 140659922):
-	result = getResult(str(i))
-	writeToFile(result)
-	print '[+] Got roll no. ' + str(i)
-	print '[+] Writing it now'
-	data = addData(data)
-data += '</table>'
+
+if __name__ == '__main__':
+    data = getInitialData()
+    num_cores = multiprocessing.cpu_count()
+    results = Parallel(n_jobs=num_cores)(delayed(getResult)(str(i)) for i in range(1714547001, 1714547076))
+    for result in results:
+        print '[+] Writing it now'
+        data = addData(data, result)
+    data += '</table>'
+    result = open('result.html', 'w+')
+    result.write(data)
+    result.close()
 
 '''
 Write the result to file
 '''
-result = open('result.html', 'w+') 
-result.write(data)
-result.close()
